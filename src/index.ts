7717e3f18fccc7a6e71f976ad18012168bc20a39
Kerren <kerren@entrostat.com>
2019-08-30 12:16:10 +0200

    Created the run in transaction function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { initialise } from './initialise';
+import { RunFunction, runInTransaction } from './run-in-transaction';
 
-export { initialise };
+export { initialise, RunFunction, runInTransaction };
---
+++ src/run-in-transaction.ts
@@ -0,0 +1,21 @@
+import { Transactional } from 'typeorm-transactional-cls-hooked';
+
+export type RunFunction = () => Promise<void> | void;
+
+export async function runInTransaction(func: RunFunction) {
+    try {
+        await TransactionCreator.run(func);
+    } catch (e) {
+        // Do nothing here, the transaction has now been rolled back.
+    }
+}
+
+class TransactionCreator {
+    @Transactional()
+    static async run(func: RunFunction) {
+        await func();
+        throw new Error(
+            `This is thrown to cause a rollback on the transaction.`,
+        );
+    }
+}
