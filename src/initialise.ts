ceebf89022ea439194bdd0a9f86c65fb21284526
Kerren <kerren@entrostat.com>
2019-08-30 12:02:32 +0200

    Added the initialise function that creates the transactional context

---
+++ src/initialise.ts
@@ -0,0 +1,13 @@
+import {
+    initializeTransactionalContext,
+    patchTypeORMRepositoryWithBaseRepository,
+} from 'typeorm-transactional-cls-hooked';
+
+/**
+ * Initialises the transaction context. See the reason for this at
+ * https://github.com/odavid/typeorm-transactional-cls-hooked
+ */
+export function initialise() {
+    initializeTransactionalContext();
+    patchTypeORMRepositoryWithBaseRepository();
+}
