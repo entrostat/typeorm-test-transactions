0cf8761a73d315dd31ff234c24baa2788a4e5c89
Kerren <kerren@entrostat.com>
2019-12-15 21:05:33 +0200

    updated libraries

--- tests/initialise-test-database.ts
+++ tests/initialise-test-database.ts
@@ -14,7 +14,7 @@ export async function initialiseTestDatabase() {
             entities: [
                 User,
             ],
-            synchronize: false,
+            synchronize: true,
             dropSchema: process.env.DB_INIT,
             logging: false,
         };
