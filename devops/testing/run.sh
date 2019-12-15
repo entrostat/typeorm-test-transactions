0d2dea80c508c454b10af95d3fab9e6ea73df02d
Kerren <kerren@entrostat.com>
2019-12-15 20:48:29 +0200

    added the run script for postgres 12

--- devops/testing/run.sh
+++ devops/testing/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "Running transactional tests over multiple database versions..."
+echo ""
+echo ""
+
 echo "Postgres 11"
 cd postgres-11
 docker-compose up --abort-on-container-exit
+
+echo "Postgres 12"
+cd postgres-12
+docker-compose up --abort-on-container-exit
+
